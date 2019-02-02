2c167269a084007d839c6b744395a86a4c563809
i008 <kubacieslik@gmail.com>
2019-02-02 08:09:26 +0100

    add retinanet

---
+++ retinanet/loss.py
@@ -0,0 +1,151 @@
+from __future__ import print_function
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from .utils import one_hot_embedding
+from torch.autograd import Variable
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+
+
+class FL2(nn.Module):
+    def __init__(self, alpha=1, gamma=2, logits=False, reduce=True):
+        super(FL2, self).__init__()
+        self.alpha = alpha
+        self.gamma = gamma
+        self.logits = logits
+        self.reduce = reduce
+
+    def forward(self, inputs, targets):
+        if self.logits:
+            BCE_loss = F.binary_cross_entropy_with_logits(inputs, targets, reduce=False)
+        else:
+            BCE_loss = F.binary_cross_entropy(inputs, targets, reduce=False)
+        pt = torch.exp(-BCE_loss)
+        F_loss = self.alpha * (1-pt)**self.gamma * BCE_loss
+
+        if self.reduce:
+            return torch.mean(F_loss)
+        else:
+            return F_loss
+        
+class FL(nn.Module):
+
+    def __init__(self, focusing_param=1, balance_param=0.25):
+        super(FL, self).__init__()
+
+        self.focusing_param = focusing_param
+        self.balance_param = balance_param
+
+    def forward(self, output, target):
+
+        cross_entropy = F.cross_entropy(output, target)
+        
+        return cross_entropy
+        cross_entropy_log = torch.log(cross_entropy)
+        logpt = - F.cross_entropy(output, target)
+        pt    = torch.exp(logpt)
+        
+        import pdb
+        pdb.set_trace()
+
+        focal_loss = -((1 - pt) ** self.focusing_param) * logpt
+
+        balanced_focal_loss = self.balance_param * focal_loss
+
+        return balanced_focal_loss
+    
+class FocalLoss(nn.Module):
+    def __init__(self, num_classes=20):
+        super(FocalLoss, self).__init__()
+        self.num_classes = num_classes
+
+    def focal_loss(self, x, y):
+        '''Focal loss.
+        Args:
+          x: (tensor) sized [N,D].
+          y: (tensor) sized [N,].
+        Return:
+          (tensor) focal loss.
+        '''
+        alpha = 0.25
+        gamma = 2
+
+        t = one_hot_embedding(y.data.cpu(), 1+self.num_classes)  # [N,21]
+        t = t[:,1:]  # exclude background
+        t = Variable(t).cuda()  # [N,20]
+
+        p = x.sigmoid().detach()
+        pt = p*t + (1-p)*(1-t)         # pt = p if t > 0 else 1-p
+        w = alpha*t + (1-alpha)*(1-t)  # w = alpha if t > 0 else 1-alpha
+        w = w * (1-pt).pow(gamma)
+        return F.binary_cross_entropy_with_logits(x, t, w, size_average=False)
+
+    def focal_loss_alt(self, x, y):
+        '''Focal loss alternative.
+        Args:
+          x: (tensor) sized [N,D].
+          y: (tensor) sized [N,].
+        Return:
+          (tensor) focal loss.
+        '''
+        alpha = 0.25
+
+        t = one_hot_embedding(y.data.cpu(), 1+self.num_classes)
+        t = t[:,1:]
+        t = Variable(t).cuda()
+
+        xt = x*(2*t-1)  # xt = x if t > 0 else -x
+        pt = (2*xt+1).sigmoid() 
+
+        w = alpha*t + (1-alpha)*(1-t)
+        loss = -w*pt.log() / 2
+        return loss.sum()
+
+    def forward(self, loc_preds, loc_targets, cls_preds, cls_targets):
+        '''Compute loss between (loc_preds, loc_targets) and (cls_preds, cls_targets).
+        Args:
+          loc_preds: (tensor) predicted locations, sized [batch_size, #anchors, 4].
+          loc_targets: (tensor) encoded target locations, sized [batch_size, #anchors, 4].
+          cls_preds: (tensor) predicted class confidences, sized [batch_size, #anchors, #classes].
+          cls_targets: (tensor) encoded target labels, sized [batch_size, #anchors].
+        loss:
+          (tensor) loss = SmoothL1Loss(loc_preds, loc_targets) + FocalLoss(cls_preds, cls_targets).
+        '''
+        batch_size, num_boxes = cls_targets.size()
+        pos = cls_targets > 0  # [N,#anchors]
+        num_pos = pos.data.long().sum()
+
+        ################################################################
+        # loc_loss = SmoothL1Loss(pos_loc_preds, pos_loc_targets)
+        ################################################################
+        mask = pos.unsqueeze(2).expand_as(loc_preds)       # [N,#anchors,4]
+        masked_loc_preds = loc_preds[mask].view(-1,4)      # [#pos,4]
+        masked_loc_targets = loc_targets[mask].view(-1,4)  # [#pos,4]
+        loc_loss = F.smooth_l1_loss(masked_loc_preds, masked_loc_targets, size_average=False)
+
+        ################################################################
+        # cls_loss = FocalLoss(loc_preds, loc_targets)
+        ################################################################
+        pos_neg = cls_targets > -1  # exclude ignored anchors
+        num_peg = pos_neg.data.long().sum()
+        mask = pos_neg.unsqueeze(2).expand_as(cls_preds)
+        masked_cls_preds = cls_preds[mask].view(-1,self.num_classes)
+        
+        cls_loss = self.focal_loss(masked_cls_preds, cls_targets[pos_neg])
+
+        pos = cls_targets > 0  # [N,#anchors]
+        num_pos = pos.data.long().sum()
+        num_pos_neg = pos_neg.data.long().sum()
+
+        if num_pos > 0:
+            loss = (cls_loss + loc_loss) / num_pos
+        elif num_pos_neg > 0:
+            loss = cls_loss
+        else:
+            raise Exception('num_pos_neg == 0')
+            
+        loss = loc_loss + cls_loss
+        return loss
---
+++ retinanet/retinanet.py
@@ -0,0 +1,147 @@
+import torch
+import torch.nn as nn
+
+from torch.autograd import Variable
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+from torchvision import models
+from math import sqrt
+import multiprocessing as mp
+
+
+class FPN(nn.Module):
+    def __init__(self, encoder_depth, pretrained=True):
+        super().__init__()
+
+        if encoder_depth == 34:
+            self.encoder = models.resnet34(pretrained=pretrained)
+            bottom_channel_nr = 512
+        elif encoder_depth == 50:
+            self.encoder = models.resnet50(pretrained=pretrained)
+            bottom_channel_nr = 2048
+        elif encoder_depth == 101:
+            self.encoder = models.resnet101(pretrained=pretrained)
+            bottom_channel_nr = 2048
+        elif encoder_depth == 152:
+            self.encoder = models.resnet152(pretrained=pretrained)
+            bottom_channel_nr = 2048
+        else:
+            raise Exception('ResNet depth must be in {34, 50, 101, 152}')
+
+        self.pool = nn.MaxPool2d(2, 2)
+
+        self.relu = nn.ReLU(inplace=True)
+
+        self.conv1 = nn.Sequential(self.encoder.conv1,
+                                   self.encoder.bn1,
+                                   self.encoder.relu,
+                                   self.pool)
+        self.conv2 = self.encoder.layer1
+        self.conv3 = self.encoder.layer2
+        self.conv4 = self.encoder.layer3
+        self.conv5 = self.encoder.layer4
+
+        self.conv6 = nn.Conv2d(bottom_channel_nr, 256, kernel_size=3, stride=2, padding=1)
+        self.conv7 = nn.Conv2d(256, 256, kernel_size=3, stride=2, padding=1)
+
+        # Lateral layers
+        self.latlayer1 = nn.Conv2d(bottom_channel_nr, 256, kernel_size=1, stride=1, padding=0)
+        self.latlayer2 = nn.Conv2d(bottom_channel_nr // 2, 256, kernel_size=1, stride=1, padding=0)
+        self.latlayer3 = nn.Conv2d(bottom_channel_nr // 4, 256, kernel_size=1, stride=1, padding=0)
+
+        # Top-down layers
+        self.toplayer1 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
+        self.toplayer2 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
+        self.toplayer3 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
+
+    def _upsample_add(self, x, y):
+        """Upsample and add two feature maps.
+        Args:
+          x: (Variable) top feature map to be upsampled.
+          y: (Variable) lateral feature map.
+        Returns:
+          (Variable) added feature map.
+        Note in PyTorch, when input size is odd, the upsampled feature map
+        with `F.upsample(..., scale_factor=2, mode='nearest')`
+        maybe not equal to the lateral feature map size.
+        e.g.
+        original input size: [N,_,15,15] ->
+        conv2d feature map size: [N,_,8,8] ->
+        upsampled feature map size: [N,_,16,16]
+        So we choose bilinear upsample which supports arbitrary output sizes.
+        """
+        _, _, H, W = y.size()
+        return F.upsample(x, size=(H, W), mode='bilinear') + y
+
+    def forward(self, x):
+        # Bottom-up
+        c1 = self.conv1(x)
+        c2 = self.conv2(c1)
+        c3 = self.conv3(c2)
+        c4 = self.conv4(c3)
+        c5 = self.conv5(c4)
+
+        p6 = self.conv6(c5)
+        p7 = self.conv7(F.relu(p6))
+
+        # Top-down
+        p5 = self.latlayer1(c5)
+        p4 = self._upsample_add(p5, self.latlayer2(c4))
+        p3 = self._upsample_add(p4, self.latlayer3(c3))
+
+        p5 = self.toplayer1(p5)
+        p4 = self.toplayer2(p4)
+        p3 = self.toplayer3(p3)
+
+        return p3, p4, p5, p6, p7
+
+class RetinaNet(nn.Module):
+    num_anchors = 9
+    
+    def __init__(self, num_classes=20):
+        super(RetinaNet, self).__init__()
+        self.fpn = FPN(50)
+        self.num_classes = num_classes
+        self.loc_head = self._make_head(self.num_anchors*4)
+        self.cls_head = self._make_head(self.num_anchors*self.num_classes)
+
+    def forward(self, x):
+        fms = self.fpn(x)
+        loc_preds = []
+        cls_preds = []
+        for fm in fms:
+            loc_pred = self.loc_head(fm)
+            cls_pred = self.cls_head(fm)
+            loc_pred = loc_pred.permute(0,2,3,1).contiguous().view(x.size(0),-1,4)                 # [N, 9*4,H,W] -> [N,H,W, 9*4] -> [N,H*W*9, 4]
+            cls_pred = cls_pred.permute(0,2,3,1).contiguous().view(x.size(0),-1,self.num_classes)  # [N,9*20,H,W] -> [N,H,W,9*20] -> [N,H*W*9,20]
+            loc_preds.append(loc_pred)
+            cls_preds.append(cls_pred)
+        return torch.cat(loc_preds,1), torch.cat(cls_preds,1)
+
+    def _make_head(self, out_planes):
+        layers = []
+        for _ in range(4):
+            layers.append(nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1))
+            layers.append(nn.ReLU(True))
+        layers.append(nn.Conv2d(256, out_planes, kernel_size=3, stride=1, padding=1))
+        return nn.Sequential(*layers)
+
+    def freeze_bn(self):
+        '''Freeze BatchNorm layers.'''
+        for layer in self.modules():
+            if isinstance(layer, nn.BatchNorm2d):
+                layer.eval()
+
+def test():
+    net = RetinaNet()
+    loc_preds, cls_preds = net(Variable(torch.randn(2,3,224,224)))
+    print(loc_preds.size())
+    print(cls_preds.size())
+    loc_grads = Variable(torch.randn(loc_preds.size()))
+    cls_grads = Variable(torch.randn(cls_preds.size()))
+    loc_preds.backward(loc_grads)
+    cls_preds.backward(cls_grads)
+
+# test()
