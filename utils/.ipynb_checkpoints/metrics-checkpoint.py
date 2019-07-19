12ec2fca101180dcb03b542736932da36876c239
i008 <kubacieslik@gmail.com>
2019-07-19 07:30:31 +0200

    up

---
+++ utils/.ipynb_checkpoints/metrics-checkpoint.py
@@ -0,0 +1,81 @@
+import numpy as np
+
+
+def dice(im1, im2, empty_score=1.0):
+    """
+    Computes the Dice coefficient, a measure of set similarity.
+    Parameters
+    ----------
+    im1 : array-like, bool
+        Any array of arbitrary size. If not boolean, will be converted.
+    im2 : array-like, bool
+        Any other array of identical size. If not boolean, will be converted.
+    Returns
+    -------
+    dice : float
+        Dice coefficient as a float on range [0,1].
+        Maximum similarity = 1
+        No similarity = 0
+        Both are empty (sum eq to zero) = empty_score
+
+    Notes
+    -----
+    The order of inputs for `dice` is irrelevant. The result will be
+    identical if `im1` and `im2` are switched.
+    """
+    im1 = np.asarray(im1).astype(np.bool)
+    im2 = np.asarray(im2).astype(np.bool)
+
+    if im1.shape != im2.shape:
+        raise ValueError("Shape mismatch: im1 and im2 must have the same shape.")
+
+    im_sum = im1.sum() + im2.sum()
+    if im_sum == 0:
+        return empty_score
+
+    # Compute Dice coefficient
+    intersection = np.logical_and(im1, im2)
+
+    return 2. * intersection.sum() / im_sum
+
+
+def calculate_acc(net, X, Y):
+    
+    net.eval()
+    
+
+    Xtorch = torch.from_numpy(X).float().cuda()
+    ytorch = torch.from_numpy(Y).float().cuda()
+
+    with torch.no_grad():
+        predictions = net(Xtorch)
+    predictions = (predictions.sigmoid() > .5).squeeze(-1)
+    predictions = np.array(predictions.cpu())
+    
+    real = np.array(ytorch.cpu())
+    acc = sum(predictions==real)/len(predictions)
+    print("model acc is: ", acc)
+    
+    net.train()
+
+
+# +
+
+def calculate_acc(net, X, Y):
+    
+    net.eval()
+    
+
+    Xtorch = torch.from_numpy(X).float().cuda()
+    ytorch = torch.from_numpy(Y).float().cuda()
+
+    with torch.no_grad():
+        predictions = net(Xtorch)
+    predictions = (predictions.sigmoid() > .5).squeeze(-1)
+    predictions = np.array(predictions.cpu())
+    
+    real = np.array(ytorch.cpu())
+    acc = sum(predictions==real)/len(predictions)
+    print("model acc is: ", acc)
+    
+    net.train()
